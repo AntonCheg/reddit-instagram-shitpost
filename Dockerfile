50e352508cae64b76ffadb95f0c5d7be9a10e9bb
Anton Nikolenko <anton.nikolenko@medentee.com>
2022-08-24 09:46:18 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+
+### Base
+FROM node:12-alpine
+
+RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
+WORKDIR /home/node/app
+
+COPY package.json package-lock.json posted_memes.json ./
+
+USER node
+
+RUN npm install
+
+COPY --chown=node:node . .
+
+EXPOSE 8080
+
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,24 @@
+const Insta = require('./inst');
+const { config } = require('dotenv');
+config();
+
+const getHour = (number) => 1000 * 60 * 60 * number;
+const interval = getHour(process.env.INTERVAL);
+const time = getHour(process.env.TIME);
+
+const main = async () => {
+  const insta = new Insta();
+  await insta.login();
+
+  const post = async () => {
+    await insta.publish();
+  };
+
+  await post();
+
+  let a = setInterval(post, interval);
+
+  setTimeout(() => clearInterval(a), time);
+};
+
+main();
